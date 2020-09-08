13fa30edd1fb671faff52502a8aa52d1df947ec7
nurlykhan <n.salamatoff@gmail.com>
2020-09-09 03:29:07 +0600

    inti

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { authRoute } = require("./auth.route");
-
-const routes = Router();
-
-routes.use("/auth", authRoute);
-
-module.exports.routes = routes;
